a466f0612cb67f6269d51a7f4efbf188dede3a8d
Gergely Vamosi <profgvamosi@gmail.com>
2025-05-12 13:42:02 +0200

    updates

--- git_cmds.sh
+++ git_cmds.sh
@@ -1,2 +1,10 @@
 git init
 git add .
+
+
+ssh-keygen -t ecdsa-sha2-nistp521 -C "gvamosi@github.com"
+#add pub key on github
+
+git remote remove master
+git remote add master git@github.com:gergelyvamosi/fileupload.git
+git push master master
